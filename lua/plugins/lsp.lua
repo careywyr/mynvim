b3d72c54f762080055114d4a239d361ac8076c39
CareyWYR <wyr95626@163.com>
2026-03-19 16:41:25 +0800

    去除warning

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@
 return {
   -- Mason: 手动安装 LSP（禁用自动安装避免冲突）
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       -- 清空 ensure_installed，改为手动安装
       ensure_installed = {},
@@ -11,7 +11,7 @@ return {
 
   -- 禁用 mason-lspconfig 的自动安装
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {},
       automatic_installation = false,
